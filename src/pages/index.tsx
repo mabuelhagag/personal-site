9e3b8a3954a8fdcb71ac81ed80fe866f581ab32d
Mohamad A <mohammed.abuelhagag@gmail.com>
2023-09-29 02:25:17 +0300

    GIGA!!!!
    Signed-off-by: Mohamad A <mohammed.abuelhagag@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,24 @@ export default function Home() {
           name="description"
           content="A personal website for Mohamad Abuelhagag(Hagag)"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
